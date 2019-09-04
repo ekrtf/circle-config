7a7914c53caf7130a2f93a6ea74253dd30c96dcc
ekratiro <ekratiro@cisco.com>
2019-09-04 16:22:06 +0200

    feat: Add test suite

---
+++ test/test.spec.js
@@ -0,0 +1,43 @@
+const { exec } = require('child_process');
+const axios = require('axios');
+
+jest.setTimeout(300000);
+
+const composeUp = () => new Promise((resolve, reject) => {
+	exec('docker-compose up -d', (err, stdout, stderr) => {
+		if (err) {
+			console.error(err);
+			return reject();
+		}
+		console.log(`stdout: ${stdout}`);
+		console.log(`stderr: ${stderr}`);
+		resolve();
+	});
+});
+
+const composeDown = () => new Promise((resolve, reject) => {
+	exec('docker-compose down', (err, stdout, stderr) => {
+		if (err) {
+			console.error(err);
+			return reject();
+		}
+		console.log(`stdout: ${stdout}`);
+		console.log(`stderr: ${stderr}`);
+		resolve();
+	});
+});
+
+describe('Dummy test', () => {
+	beforeEach(async () => {
+		await composeUp();
+	});
+
+	afterEach(async () => {
+		await composeDown();
+	});
+
+	it('should say pong', async () => {
+		const res = await axios.get('http://localhost:8080/ping');
+		expect(res.data).toBe('Pong!');
+	});
+});
